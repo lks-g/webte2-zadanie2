2ebdb027d0e867be80c9387765dc80d2ff331783
lks <endygamessk@gmail.com>
2023-04-04 21:46:03 +0200

    modified parse_data

--- php/parse_data.php
+++ php/parse_data.php
@@ -2,6 +2,102 @@
 
 require_once('../config.php');
 
+function parse_freefood_menu($menu_source_code) {
+    $dom = new DOMDocument();
+    $dom->loadHTML($menu_source_code);
+
+    $menu_type = $dom->getElementsByTagName("h4")->item(0)->nodeValue;
+    $menu_items = $dom->getElementsByTagName("li");
+
+    $dishes = array();
+    foreach ($menu_items as $menu_item) {
+        $day_title = $menu_item->getElementsByTagName("span")->item(0)->nodeValue;
+        $date = DateTime::createFromFormat('l, j.n.Y', $day_title);
+        if (!$date) {
+            continue;
+        }
+        $offer_items = $menu_item->getElementsByTagName("li");
+        foreach ($offer_items as $offer_item) {
+            $name = $offer_item->getElementsByTagName("span")->item(1)->nodeValue;
+            $price_node = $offer_item->getElementsByTagName("span")->item(2);
+            $price = trim(str_replace('€', '', $price_node->nodeValue));
+            $dishes[] = array(
+                'menu_type' => $menu_type,
+                'name' => $name,
+                'price' => $price,
+                'location' => 'FreeFood',
+                'image_url' => null
+            );
+        }
+    }
+    return $dishes;
+}
+
+function parse_venza_menu($menu_source_code) {
+    $dom = new DOMDocument();
+    $dom->loadHTML($menu_source_code);
+
+    $menu_type = $dom->getElementsByTagName("h5")->item(0)->nodeValue;
+    $menu_items = $dom->getElementsByTagName("li");
+
+    $dishes = array();
+    foreach ($menu_items as $menu_item) {
+        $name = $menu_item->getElementsByTagName("h5")->item(0)->nodeValue;
+        $price = $menu_item->getElementsByTagName("h5")->item(1)->nodeValue;
+        $location = $dom->getElementsByTagName("p")->item(0)->nodeValue . ', ' . $dom->getElementsByTagName("p")->item(1)->nodeValue;
+        $dishes[] = array(
+            'menu_type' => $menu_type,
+            'name' => $name,
+            'price' => $price,
+            'location' => $location,
+            'image_url' => null
+        );
+    }
+    return $dishes;
+}
+
+function parse_eatmeet_menu($menu_source_code) {
+    $dom = new DOMDocument();
+    $dom->loadHTML($menu_source_code);
+
+    $menu_type = $dom->getElementsByTagName("h4")->item(0)->nodeValue;
+    $menu_items = $dom->getElementsByTagName("div");
+
+    $dishes = array();
+
+    $location_items = $dom->getElementsByTagName("div");
+    $location = "";
+    foreach ($location_items as $location_item) {
+        if ($location_item->getAttribute('class') == 'contact') {
+            $location_nodes = $location_item->getElementsByTagName("p");
+            $location_arr = array();
+            foreach ($location_nodes as $location_node) {
+                $location_arr[] = trim($location_node->nodeValue);
+            }
+            $location = implode(", ", array_slice($location_arr, 0, 2));
+            break;
+        }
+    }
+
+    foreach ($menu_items as $menu_item) {
+        if ($menu_item->getAttribute('class') == 'menu-body menu-left  menu-white ') {
+            $name = trim($menu_item->getElementsByTagName("p")->item(0)->nodeValue);
+            $price = $menu_item->getElementsByTagName("span")->item(0)->nodeValue;
+            $img_node = $menu_item->getElementsByTagName("img")->item(0);
+            $img_url = $img_node ? $img_node->getAttribute('src') : null;
+
+            $dishes[] = array(
+                'menu_type' => $menu_type,
+                'name' => $name,
+                'price' => $price,
+                'location' => $location,
+                'image_url' => $img_url
+            );
+        }
+    }
+    return $dishes;
+}
+
 try {
     $db = new PDO("mysql:host=$hostname;dbname=$dbname;charset=utf8mb4", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -15,47 +111,22 @@ try {
     echo $e->getMessage();
 }
 
-// Loop through the menus and parse the source code for each provider
 foreach ($menus as $menu) {
-    switch ($menu['provider_id']) {
-        case 1: // FreeFood
-            // Parse the source code for FreeFood
-            // ...
+    switch ($menu['name']) {
+        case "FreeFood":
+            $freefood_dishes = parse_freefood_menu($menu['source_code']);
+            $dishes = array_merge($dishes, $freefood_dishes);
             break;
-        case 2: // Eat&Meet
-            // Parse the source code for Eat&Meet
-            // ...
+        case "Eat&Meet":
+            $eatmeet_dishes = parse_eatmeet_menu($menu['source_code']);
+            $dishes = array_merge($dishes, $eatmeet_dishes);
             break;
-        case 3: // Venza
-            // Parse the source code for Venza
-            $venza_dom = new DOMDocument();
-            $venza_dom->loadHTML($menu['source_code']);
-
-            // Find the menu items
-            $venza_items = $venza_dom->getElementsByTagName("li");
-
-            // Loop through the menu items and extract the name and price for each dish
-            $venza_dishes = array();
-            foreach ($venza_items as $venza_item) {
-                $name = $venza_item->getElementsByTagName("h5")->item(0)->nodeValue;
-                $price = $venza_item->getElementsByTagName("h5")->item(1)->nodeValue;
-                $venza_dishes[] = array("name" => $name, "price" => $price);
-            }
-
-            // Find the location information
-            $venza_location = $venza_dom->getElementsByTagName("div")->item(2)->getElementsByTagName("p")->item(0)->nodeValue;
-            $venza_address = $venza_dom->getElementsByTagName("div")->item(2)->getElementsByTagName("p")->item(1)->nodeValue;
-
-            // Output the results
-            echo "Menu for Venza:\n";
-            foreach ($venza_dishes as $dish) {
-                echo $dish['name'] . " - " . $dish['price'] . "\n";
-            }
-            echo "Location for Venza: " . $venza_location . "\n";
-            echo "Address for Venza: " . $venza_address . "\n";
+        case "Venza":
+            $venza_dishes = parse_venza_menu($menu['source_code']);
+            $dishes = array_merge($dishes, $venza_dishes);
             break;
         default:
-            // Unsupported provider_id
+            echo "Error: Provider does not exist.";
             break;
     }
 }
